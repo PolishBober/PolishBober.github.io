b0482177ff3186b3b6ae7ccdd85581837a0dc288
PolishBober <polishbober@gmail.com>
2023-05-17 21:19:06 +0000

    selenium in python failed

---
+++ link.py
@@ -0,0 +1,9 @@
+#site = input('Site address: ')
+#login = input('Login: ')
+#password = input('Password: ')
+#exercise = input('Exercise name: ')
+
+import os
+
+os.system('echo test > test.txt')
+print('test')
